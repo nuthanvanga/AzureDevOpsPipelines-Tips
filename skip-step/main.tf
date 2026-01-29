d8904b2971b4e77264a8fc7df916b82052e0d6fa
nuthanvanga <36616434+nuthanvanga@users.noreply.github.com>
2026-01-29 11:46:38 +0530

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -1,15 +1,17 @@
 terraform {
-  backend "azurerm" {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.58.0"
+    }
   }
 }
 
 provider "azurerm" {
-  version = ">=2.0"
-  # The "feature" block is required for AzureRM provider 2.x.
-  features {}
+  # Configuration options
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "resourcegroup-test-tbd-3"
-  location = "westeurope"
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
 }
